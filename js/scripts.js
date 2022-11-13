69055eb59101744cc0b13867b4506fc6c3b7335e
kleberandradejua <kleberandrade98@gmail.com>
2022-11-13 15:35:53 -0300

    Primeiras aulas do curso, sintaxe, variávei e strings

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+//Isso é um comentário de uma linha
+/*
+ Isso é um comentário de várias linhas
+ */
+
+/* console.log('Mensagem enviada pelo console')
+console.log('Mensagem enviada pelo console')
+console.log('Mensagem enviada pelo console')
+console.log('Mensagem enviada pelo console')
+ */
+
+//Aula sobre variáveis
+var latitude = 40.87663;
+var longitude = -8.08373;
+
+console.log(latitude)
+console.log(longitude)
+
+
+//Aula Tipos de dados - string
+
+var nome = "João";
+var sobrenome = "das Neves";
+var cpf = "123.321.213.12";
+var telefone = "99889887";
+var ddd = "88";
+var email = "kleberandrade98@gmail.com";
+
+console.log(nome + ' ' + sobrenome)
+console.log('Primeira letra do nome: ' + nome[0])
+
+console.log('Número de caracteres do tele fone: ' + telefone.length)
