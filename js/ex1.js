264100b69677a18d8c407662d8661d2bf7af6b73
kleberandradejua <kleberandrade98@gmail.com>
2022-11-14 20:07:43 -0300

    Finalizando a primeira seção do curso de js básico.Tipos de dados, number, boolean, null e undefined

---
+++ js/ex1.js
@@ -0,0 +1,60 @@
+/*alert('O arquivo ex1.js está funcionando')
+
+var nome = "Kleber de Andrade";
+var nasc = 2022 - 1994;
+
+console.log('Olá me chamdo ' + nome + ', tenho ' + nasc + ' anos e estou estudando Javascript')
+
+var aluno = "João Feijão";
+var matricula = "98754";
+let n1 = 9.5;
+let n2 = 7.9;
+
+media = (n1 + n2)/2;
+
+console.log('O aluno '+ aluno + ', matrícula ' + matricula + ', obteve a média final ' + media);*/
+
+
+var numtel = "87654321";
+console.log(numtel.length)
+
+if(numtel.length == 9){
+    console.log('Resultado do teste: TRUE')
+} else{
+    console.log('Resultado do teste: FALSE')
+}
+
+var pot = 32;
+console.log(Math.pow(pot, 6))
+
+/*
+Resposta da 6ª questão
+letra c não pode ser variável pois é uma palavra reservada da linguagem, que identifica o tipo de uma variável;
+letra d não pode ser variável pois possui um hifen, que não é adqueado para nomear variáveis;
+letra f não pode ser variável pois não se deve iniciar nome de variáveis com números;
+letra g não se pode usar espaço em nomes de variáveis
+*/
+
+var numero = 6;
+console.log((7+5)/numero + 2)
+
+/*
+Resposta da 7ª questão
+1 - será apresentado a concatenação dos números 25: 2525;
+2 - o resultado será 4, pois será resolvido as operações primordiais, o que tá entre parenteses, depois a divisão e após, a soma;
+3 - será apresentado no console undefined, pois não se tem valor na variável;
+4 - será apresentado o número 40 na tela;
+5 - será apresentado o tipo da variável pressão;
+6 - será apresentado o tipo da variável temperatura;
+*/
+
+
+/*
+Resposta da 8ª questão
+1 - compara se 65 não é igual a 65;
+2 - compara se idade é maior ou igual a 65;
+3 - compara se a string 65 é igual a 65;
+4 - compara se 65 não é igual a idade em tipo e valor;
+5 - tipo da comparação de idade maior que 60, no caso true, boolean;
+6 - tipo da comparação de idade maior que 70, no caso false, boolean;
+*/
--- js/scripts.js
+++ js/scripts.js
@@ -29,4 +29,34 @@ var email = "kleberandrade98@gmail.com";
 console.log(nome + ' ' + sobrenome)
 console.log('Primeira letra do nome: ' + nome[0])
 
-console.log('Número de caracteres do tele fone: ' + telefone.length)
+console.log('Número de caracteres do telefone: ' + telefone.length)
+
+//Aula tipos de dados - numbers
+
+var num1 = 20;
+var num2 = 3;
+var soma = num1 + num2;
+var subtracao = num1 - num2;
+var divisao = num1 / num2;
+var multiplicacao = num1 * num2;
+var media = (num1 + num2) / 2;
+
+console.log(media)
+console.log(multiplicacao)
+
+console.log(Math.pow(2,6))
+
+increment = num1;
+increment += 5;
+
+console.log(increment)
+
+
+//tipo de uma variavel
+
+var vtype = 'teste';
+console.log(typeof vtype)
+
+var typede;
+console.log(typeof typede)
+
